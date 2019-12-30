3d2635359ad9d0e2f857b57563d60e0d90f4dc6b
郑晗 <han.zheng@51zouchuqu.com>
2019-12-30 10:28:04 +0800

    no message

---
+++ ZCQFramework/ZCQFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  ZCQFrameworkTest.h
+//  zcqappForC
+//
+//  Created by 郑晗 on 2019/12/28.
+//  Copyright © 2019 zouchuqu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZCQFrameworkTest : NSObject
+
++ (void)frameWorkTest;
+
+@end
+
+NS_ASSUME_NONNULL_END
